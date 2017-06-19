666978ebf1b1c08cdba3be072056cfbef29d230d
bote795 <nbotell2@gmail.com>
2017-06-18 22:39:59 -0700

    port

--- hello.py
+++ hello.py
@@ -34,4 +34,5 @@ def login():
         return jsonify({"status": "fail"})
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
